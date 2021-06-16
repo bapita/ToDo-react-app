ba3eb30f1494293b60d839749b1b4bfa85f14d8a
bapita <bapitaroy14@gmail.com>
2021-06-16 14:08:13 +0530

    added commands for deployment

--- src/component/Todoapp.jsx
+++ src/component/Todoapp.jsx
@@ -5,26 +5,63 @@ import todo from "../images/todo.svg";
 const Todoapp = () => {
     const [inputData, setInputData] = useState("");
     const[task, setTask] = useState([]);
+    const[toggleSubmit, setToggleSubmit] = useState(true);
+    const[isEditItem, setIsEditItem] = useState(null);
 
+/************** Add Task ***************/
     const addTask = () => {
         if(!inputData) {
             alert("Empty feild can not be a task. LOL!")
-        }else{
-            setTask([...task,inputData]); // to save previous data's state we used spread operator i.e. ...
+        }else if(inputData && !toggleSubmit){
+            setTask(
+                task.map((currentElem) => {
+                    if(currentElem.id === isEditItem) {
+                        return {
+                            ...currentElem,
+                            name: inputData
+                        }
+                    }
+                    return currentElem;
+                })
+            )
+                setToggleSubmit(true);
+                setInputData('');
+                setIsEditItem(null);
+        }
+        else{
+            const allInputData = {
+                id: new Date().getTime().toString(), // to get unique ID 
+                name: inputData
+            }
+            setTask([...task,allInputData]); // to save previous data's state we used spread operator i.e. ...
             setInputData('');
         }
     }
 
-    const deleteTask = (id) => {
-        const updatedTasks = task.filter((currElm, index) => {
-            return index !== id;
+/************** Edit Task ***************/
+    const editTask = (index) => {
+        let newEdittask = task.find((currentElem) => {
+            return currentElem.id === index
+        })
+        setToggleSubmit(false);
+        setInputData(newEdittask.name);
+        setIsEditItem(index);
+    }
+
+/************** Delete Task ***************/
+    const deleteTask = (index) => {
+        const updatedTasks = task.filter((currElm) => {
+            return currElm.id !== index;
         })
         setTask(updatedTasks);
     }
 
+
+/************** Delete / Remove All ***************/
     const removeAll = () => {
         return setTask([]);
     }
+/************** End of Functions ***************/    
     return(
         <>
             <div className="main-div">
@@ -39,19 +76,30 @@ const Todoapp = () => {
                         placeholder=" ✍️ Write Tasks Here"
                         value={inputData}
                         onChange={(event) =>  setInputData(event.target.value) } />
-                        <i className="fa fa-plus add-btn" title="Add Tasks" onClick={addTask}></i>
+                        {
+                            toggleSubmit ? <i className="fa fa-plus add-btn" title="Add Tasks" onClick={addTask}></i> : 
+                            <i className="far fa-edit add-btn" title="Edit Task" onClick={addTask}></i> 
+                        }
+                        
                     </div>
                     <div className="showItems">
 
                         {
-                            task.map((currentElement,index) => {
+                            task.map((currentElement) => {
                                 return(
                                     <>
-                                        <div className="eachItem" key={index}>
-                                            <h3>{currentElement}</h3>
-                                            <i className="far fa-trash-alt add-btn"
-                                            title="Delete Tasks"
-                                            onClick={() => deleteTask(index)}></i>
+                                        <div className="eachItem" key={currentElement.id}>
+                                            <h3>{currentElement.name}</h3>
+                                            <div className="todo-btn">
+
+                                                <i className="far fa-edit add-btn"
+                                                title="Edit Tasks"
+                                                onClick={() => editTask(currentElement.id)}></i>
+                                            
+                                                <i className="far fa-trash-alt add-btn"
+                                                title="Delete Tasks"
+                                                onClick={() => deleteTask(currentElement.id)}></i>
+                                        </div>
                                         </div>
                                     </>
                                 )
